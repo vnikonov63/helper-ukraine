9ba966f6ece294072249e633257a469be609625f
vnikonov63 <nikonov.va@yandex.ru>
2022-04-09 06:30:03 -0700

    0.0.8

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -8,7 +8,7 @@ render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path="admin" element={<Admin/>} />
+      <Route path="/admin" element={<Admin/>} />
     </Routes>
   </BrowserRouter>,
   rootElement
